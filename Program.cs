fa3411b5fb3bbc3b37c56644aa2d3e500d0fb906
maxie1337 <lonnstrom.max@gmail.com>
2025-01-10 09:53:12 +0100

    SaleItemController tillagd

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using Auktioner.Models;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseInMemoryDatabase("AuctionDatabase"));
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-.AddEntityFrameworkStores<AppDbContext>();
+var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
